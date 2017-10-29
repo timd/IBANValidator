1c70e1fcb0c8d8d6e3b619278f22592f4ac8726a
Tim Duckett <tim.duckett@oetkerdigital.com>
2017-10-29 15:27:51 +0100

    Initial Commit

---
+++ IBANValidator/IBANValidator.h
@@ -0,0 +1,19 @@
+//
+//  IBANValidator.h
+//  IBANValidator
+//
+//  Created by Tim Ducket on 29.10.17.
+//  Copyright © 2017 Tim Duckett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBANValidator.
+FOUNDATION_EXPORT double IBANValidatorVersionNumber;
+
+//! Project version string for IBANValidator.
+FOUNDATION_EXPORT const unsigned char IBANValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBANValidator/PublicHeader.h>
+
+
